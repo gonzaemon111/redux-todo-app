41151e4bb0ae69629610f326f3395cf915dc4714
gonzaemon111 <goki14914@icloud.com>
2019-09-24 19:15:26 +0900

    add components/todos/inputs.jsx

--- src/components/todos/inputs.jsx
+++ src/components/todos/inputs.jsx
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
 
 class Inputs extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.props.addTodo("新規Todo");
+  }
+
   render() {
     return (
       <div>
         <input placeholder="新規TODOを入力してください"></input>
-        <button>登録</button>
+        <button onClick={this.handleClick}>登録</button>
       </div>
     );
   }
