95cf9faf6bd955828db054499153fe37aaf03bb2
gonzaemon111 <goki14914@icloud.com>
2019-09-24 18:41:17 +0900

    add src/pages/App.js

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from "../layouts/header";
+import Inputs from '../components/todos/inputs';
+import List from '../components/todos/list';
 
-function App() {
-  return (
-    <div>
-      <Header></Header>
-      <h1>Hello World!</h1>
-    </div>
-  );
+class App extends Component {
+  render () {
+    const tasks = [
+      { title: 'Todo１つ目', id: 0 },
+      { title: 'Todo２つ目', id: 1 },
+    ];
+
+    return (
+      <div>
+        <Header></Header>
+        <Inputs />
+        <List tasks={tasks}></List>
+        <h1>Hello World!</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
