7469b4c44e2906ec351a83930c446c548a6e2992
gonzaemon111 <goki14914@icloud.com>
2019-09-24 18:50:56 +0900

    add layouts/header.jsx

--- src/layouts/header.jsx
+++ src/layouts/header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 class Header extends React.Component {
   render() {
     return (
-      <header><p>Header</p></header>
+      <header><h1>Header</h1></header>
     );
   }
 }
